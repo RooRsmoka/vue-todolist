07fa8cc6228ee5ddc9b36fa90df6b481c85322e7
RooRsmoka <chri.disa96@gmail.com>
2022-01-17 16:11:45 +0100

    completamento lista oggetti data e formattazione

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        
+        todoList: [
+            {
+                text: 'Fare la spesa',
+                done: true,
+            },
+            {
+                text: 'Portare la macchina dal carrozziere',
+                done: true,
+            },
+            {
+                text: 'Svuotare la lavastoviglie',
+                done: false,
+            },
+        ]
     },
 })
