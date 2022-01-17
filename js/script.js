869eb3d8077edea552951818897b9a196a47fa7d
RooRsmoka <chri.disa96@gmail.com>
2022-01-17 21:04:39 +0100

    completata Milestone 3 + bonus 1

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 new Vue({
     el: '#app',
     data: {
+        heading: 'TodoList',
         todoList: [
             {
                 text: 'Fare la spesa',
                 done: true,
             },
             {
-                text: 'Portare la macchina dal carrozziere',
+                text: 'Fare il bucato',
                 done: true,
             },
             {
                 text: 'Svuotare la lavastoviglie',
                 done: false,
             },
-        ]
+        ],
+        newAction: '',
     },
     methods: {
+        addAction: function() {
+            this.todoList.push({text: this.newAction, done: false});
+            this.newAction = '';
+        },
         removeAction: function(index) {
             this.todoList.splice(index, 1)
         }
